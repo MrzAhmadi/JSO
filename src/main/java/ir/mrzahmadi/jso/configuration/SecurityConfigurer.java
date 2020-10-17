ca0eae9373f4a7548273d1f697fe072502ccdafe
Mohammadreza <smrahmadi12@gmail.com>
2020-10-17 13:31:25 +0330

    add some configuration

---
+++ src/main/java/ir/mrzahmadi/jso/configuration/SecurityConfigurer.java
@@ -0,0 +1,20 @@
+package ir.mrzahmadi.jso.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/**").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
