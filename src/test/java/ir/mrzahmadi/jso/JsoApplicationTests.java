5738434f0d50faa13d85fa9e2c571ec0a14f0e77
Mohammadreza <smrahmadi12@gmail.com>
2020-10-17 12:58:45 +0330

    Initial commit

---
+++ src/test/java/ir/mrzahmadi/jso/JsoApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mrzahmadi.jso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
